09b796d272f510b8f987d564d69d4d97d3be8ec0
janWICKEDpro <janroyal45@gmail.com>
2023-11-05 06:48:33 +0100

    more packages and structure

--- lib/main.dart
+++ lib/main.dart
@@ -43,15 +43,17 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(
+          widget.title,
+          style: const TextStyle(fontFamily: 'DmSans'),
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            const Text('You have pushed the button this many times:',
+                style: const TextStyle(fontFamily: 'DmSans')),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
