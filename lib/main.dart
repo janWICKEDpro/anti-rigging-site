3c01b8c63bf60fd2a72a8c0f36c1c742c85f9017
janWICKEDpro <janroyal45@gmail.com>
2023-11-05 07:08:53 +0100

    core packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:anti_rigging/firebase_options.dart';
+import 'package:anti_rigging/locator.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  getIt.registerSingleton(DefaultFirebaseOptions());
+
+  await Firebase.initializeApp(
+    options: getIt<DefaultFirebaseOptions>().currentPlatform,
+  );
   runApp(const MyApp());
 }
 
