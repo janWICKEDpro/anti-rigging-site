796ef13a729384787af125cf424998e923d0b846
janWICKEDpro <janroyal45@gmail.com>
2023-11-07 20:08:55 +0100

    started a create election widget

---
+++ lib/admin_dashboard/view/create_election.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateElection extends StatefulWidget {
+  const CreateElection({super.key});
+
+  @override
+  State<CreateElection> createState() => _CreateElectionState();
+}
+
+class _CreateElectionState extends State<CreateElection> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
