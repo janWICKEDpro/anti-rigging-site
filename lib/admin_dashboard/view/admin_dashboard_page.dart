0f99210d3e8c1d520b8f7672b917e06a0a66d4bc
janWICKEDpro <janroyal45@gmail.com>
2023-11-14 20:20:06 +0100

    bug fix

---
+++ lib/admin_dashboard/view/admin_dashboard_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminDashBoardPage extends StatefulWidget {
+  const AdminDashBoardPage({super.key});
+
+  @override
+  State<AdminDashBoardPage> createState() => _AdminDashBoardPageState();
+}
+
+class _AdminDashBoardPageState extends State<AdminDashBoardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/admin_dashboard/view/utils.dart
+++ lib/admin_dashboard/view/utils.dart
@@ -1,4 +1,5 @@
+import 'package:anti_rigging/admin_dashboard/view/admin_dashboard_page.dart';
 import 'package:anti_rigging/admin_dashboard/view/election_list.dart';
 import 'package:flutter/material.dart';
 
-List<Widget> adminPages = [Container(), const ElectionList()];
+List<Widget> adminPages = [AdminDashBoardPage(), const ElectionList()];
