d3fa666b5ed05f6225380bd474d03605d96948ac
janWICKEDpro <janroyal45@gmail.com>
2023-11-19 08:51:39 +0100

    voting logic simplified

--- lib/user_dashboard/bloc/user_dashboard_state.dart
+++ lib/user_dashboard/bloc/user_dashboard_state.dart
@@ -5,9 +5,11 @@ class UserDashboardState {
   final Election? election;
   final FetchInfo fetchInfo;
   final FetchVoteList fetchVoteList;
+  final List<(String, List<Candidate>, bool)>? voteList;
 
   UserDashboardState({
     this.user,
+    this.voteList,
     this.election,
     this.fetchInfo = FetchInfo.loading,
     this.fetchVoteList = FetchVoteList.loading,
@@ -17,8 +19,10 @@ class UserDashboardState {
       {AppUser? user,
       FetchInfo? fetchInfo,
       FetchVoteList? fetchVoteList,
+      List<(String, List<Candidate>, bool)>? voteList,
       Election? election}) {
     return UserDashboardState(
+        voteList: voteList ?? this.voteList,
         user: user ?? this.user,
         election: election ?? this.election,
         fetchInfo: fetchInfo ?? this.fetchInfo,
