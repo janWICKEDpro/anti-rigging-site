21cfb0a202b8cdf085d8b756d8509dc923d393fd
janWICKEDpro <janroyal45@gmail.com>
2023-11-19 13:41:16 +0100

    progress

--- lib/user_dashboard/view/vote_view.dart
+++ lib/user_dashboard/view/vote_view.dart
@@ -1,8 +1,10 @@
+import 'package:anti_rigging/user_dashboard/bloc/enums.dart';
 import 'package:anti_rigging/user_dashboard/bloc/user_dashboard_bloc.dart';
 import 'package:anti_rigging/utils/colors.dart';
 import 'package:anti_rigging/utils/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class VoteView extends StatefulWidget {
   const VoteView({super.key});
@@ -23,6 +25,60 @@ class _VoteViewState extends State<VoteView> {
               'You May Now Cast Your Votes',
               style: AppTextStyles().headers.copyWith(color: primaryColor),
             ),
+            Builder(builder: (context) {
+              if (state.fetchVoteList == FetchVoteList.loading) {
+                return Expanded(
+                  child: SizedBox(
+                    child: Center(
+                      child: Column(
+                        children: [
+                          const Text('Loading Votes'),
+                          LoadingAnimationWidget.hexagonDots(
+                              color: primaryColor, size: 60)
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              } else if (state.fetchVoteList == FetchVoteList.failed) {
+                return Expanded(
+                  child: SizedBox(
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Text(
+                            'Loading Votes',
+                            style: AppTextStyles()
+                                .normal
+                                .copyWith(color: Colors.redAccent),
+                          ),
+                          OutlinedButton(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all(primaryColor)),
+                              onPressed: () {
+                                context
+                                    .read<UserDashboardBloc>()
+                                    .add(OnVoteListFetched());
+                              },
+                              child: Text(
+                                'Retry',
+                                style: AppTextStyles()
+                                    .normal
+                                    .copyWith(color: Colors.white),
+                              ))
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }
+              return Expanded(child: SizedBox(
+                child: ListView.builder(itemBuilder: (context, index) {
+                  return Container();
+                }),
+              ));
+            })
           ],
         );
       },
