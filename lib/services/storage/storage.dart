ca3327534efd10ea9bb979ba56d5a968a6be8aae
janWICKEDpro <janroyal45@gmail.com>
2023-11-27 19:53:01 +0100

    added shared preferences

--- lib/services/storage/storage.dart
+++ lib/services/storage/storage.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'dart:math' as math;
 import 'dart:typed_data';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:file_picker/file_picker.dart';
@@ -6,6 +7,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class Storage {
   final _dbStorage = FirebaseStorage.instance;
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   Future<String> storeImage(PlatformFile file) async {
     Uint8List fileBytes = file.bytes!;
     try {
@@ -17,7 +19,23 @@ class Storage {
     }
   }
 
-  Future cacheRandomInt() async {
-    final prefs = await SharedPreferences.getInstance();
+  Future cacheSession() async {
+    try {
+      final SharedPreferences prefs = await _prefs;
+      int session = math.Random().nextInt(1000000) + 1000000;
+      return await prefs.setInt('session', session);
+    } catch (e) {
+      throw '$e';
+    }
+  }
+
+  Future<int?> getCachedSession() async {
+    try {
+      final SharedPreferences prefs = await _prefs;
+      int session = math.Random().nextInt(1000000) + 1000000;
+      return prefs.getInt('session');
+    } catch (e) {
+      throw '$e';
+    }
   }
 }
